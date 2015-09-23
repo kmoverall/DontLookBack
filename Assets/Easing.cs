8a209cdabe6146b247492cd68dbd0099c4d4fef8
Kevin Overall <kmoverall@gmail.com>
2015-09-22 20:52:46 -0400

    Tweaked commit, not sure if old message is still there

--- Assets/Easing.cs
+++ Assets/Easing.cs
@@ -1,6 +1,100 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum EasingType {Linear, Quadratic, Sinusoidal, Exponential, Circular, Cubic, Quartic, Quintic, Back, Elastic, Bounce}
+
 public class Easing {
+	public static float evaluate(float x, float t, float start, float change, float duration, EasingType inType, EasingType outType) {
+
+		return 0.0f;
+	}
+
+	private float linIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float linOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float quadIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float quadOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float sinIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float sinOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float expIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float expOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float circIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float circOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float cubIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float cubOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float quarIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float quarOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float quintIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float quintOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float backIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float backOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float elasIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float elasOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float bouncIn(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
+
+	private float bounceOut(float x, float t, float start, float change, float duration) {
+		return 0.0f;
+	}
 
 }
